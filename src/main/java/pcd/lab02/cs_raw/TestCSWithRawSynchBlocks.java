caa0a5c58ece904a7a799b5426fc8a32af5595e5
Cristina Zoccola <cristina.zoccola@studio.unibo.it>
2025-03-03 15:09:20 +0100

    done exercise critical sections

--- src/main/java/pcd/lab02/cs_raw/TestCSWithRawSynchBlocks.java
+++ src/main/java/pcd/lab02/cs_raw/TestCSWithRawSynchBlocks.java
@@ -1,7 +1,7 @@
 package pcd.lab02.cs_raw;
 
 public class TestCSWithRawSynchBlocks {
-
+// it will never print a sequence where b1,b2 has a2 and or a3 in between and vice versa (I think)
 	public static void main(String[] args) {
 
 		var lock = new Object();	
